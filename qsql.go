b9b581d9642f36ac6d22f73a2729a18aa498fab1
Andrew <amh11706@gmail.com>
2022-01-23 10:44:20 +0100

    disable db retry

--- qsql.go
+++ qsql.go
@@ -1,6 +1,7 @@
 package qsql
 
 import (
+	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,21 +14,20 @@ func Connect(driver, host, prefix string, logger func(...interface{})) *sqlx.DB 
 	db, err := sqlx.Open(driver, host)
 	if err != nil {
 		logger(prefix + " failed to connect: " + err.Error())
-		time.Sleep(5 * time.Second)
-		return Connect(driver, host, prefix, logger)
+		os.Exit(1)
+		return nil
 	}
 	db.SetMaxIdleConns(400)
 	db.SetMaxOpenConns(400)
 	db.SetConnMaxLifetime(20 * time.Minute)
 
-	for {
-		if err = db.Ping(); err != nil {
-			logger(prefix + " failed to connect: " + err.Error())
-		} else {
-			logger(prefix+" connected to DB in", time.Since(start))
-			return db
-		}
-
-		time.Sleep(10 * time.Second)
+	if err = db.Ping(); err != nil {
+		logger(prefix + " failed to connect: " + err.Error())
+	} else {
+		logger(prefix+" connected to DB in", time.Since(start))
+		return db
 	}
+
+	os.Exit(1)
+	return nil
 }
